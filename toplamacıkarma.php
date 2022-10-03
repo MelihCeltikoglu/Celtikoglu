06007fbc6e36dcc10bfa78f3e68c87b5f51edb2b
MelihCeltikoglu <114668376+MelihCeltikoglu@users.noreply.github.com>
2022-10-03 15:47:58 +0300

    odev
    toplama, cikarma , bolme, carpma odevi.

---
+++ toplamacıkarma.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+
+<?php
+
+
+$sayi1 = 10;
+$sayi2 = 5;
+
+
+echo " Toplama İslemi Sonucu : "     .($sayi1 + $sayi2 )."<br />" ;
+echo " Cıkarma İslemi Sonucu : "     .($sayi1 - $sayi2 )."<br />";
+echo " Bolme İslemi Sonucu :    "    .($sayi1  / $sayi2 )."<br />" ;
+echo " Carpma İslemi Sonucu :  "     .($sayi1 * $sayi2 )."<br />";
+?>
+
+
+
+
+
+
+
+</body>
+</html>
